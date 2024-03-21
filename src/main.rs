b8c50145aa194357fb6b36b8f8573821bb5d4ccc
kaisic1224 <hiddenshadows1224@gmail.com>
2024-03-21 03:23:35 -0400

    save

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub type Error = Box<dyn std::error::Error>;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-fn main() -> Result<()>{
-       todo!() 
+fn main() -> (){
+    println!("jackie chan");
+        
 }
